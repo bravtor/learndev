58b32f4c1e780435e1586cc0f115ad0976522fff
Mac book pro Johann <macbookprojohann@MacBook-Pro-de-Mac.local>
2022-12-19 22:11:45 +0100

    deploy test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          LearnDev.app
         </p>
         <a
           className="App-link"
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         test juste avec un deploy encore une fois et toujours
+      
         </a>
       </header>
     </div>
